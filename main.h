f06ad0707466e908d53c271c22627ac7c5c174d1
Alagwudavid <alagwudavid2019@gmail.com>
2023-06-28 22:01:14 -0700

    printf - 024

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef _HEADER_
 #define _HEADER_
 
-/*
+/**
  * inclusion
  */
 #include <stdlib.h>
@@ -9,10 +9,31 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
- * prototypes
+/**
+  * struct specifiers - Struct specifiers
+  * @specifier: The conversion specifier
+  * @f: The function pointer
+  */
+typedef struct specifiers
+{
+	char *specifier;
+	int (*f)(va_list args);
+} spc_dt;
+
+/**
+ * prototype
  */
+int _write(char c);
 int _printf(const char *format, ...);
-int _putchar(char c);
+int _print_a_char(va_list args);
+int _print_a_string(va_list args);
+int _print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
+int _print_a_integer(va_list args);
+void _recursion_integer(int a);
+int _print_int_binary(va_list args);
+void _recursion_int_binary(int a);
+int _validate_char(char _type);
 
 #endif
